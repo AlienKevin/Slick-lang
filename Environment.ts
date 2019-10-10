a7691afddca27336eaadf3ef402caf8f4d911d2e
Kevin Li <kevinli020508@gmail.com>
2019-10-10 17:23:10 -0400

    Create Environment for parser to keep track of variables

---
+++ Environment.ts
@@ -0,0 +1,46 @@
+import { Token } from "./Token";
+
+interface Value {
+    mutable: boolean,
+    read: boolean
+}
+
+export class Environment {
+    private values: { [name: string]: Value } = Object.create(null);
+    constructor(private error: Function, private enclosing?: Environment) {}
+
+    lookup(nameToken: Token): boolean {
+        const name = nameToken.lexeme;
+        if (this.isDeclared(name)) {
+            return true;
+        }
+        if (this.enclosing !== undefined) {
+            return this.enclosing.lookup(nameToken);
+        }
+        return false;
+    }
+
+    isDeclared(name: string) {
+        return this.values[name] !== undefined;
+    }
+
+    declarePrimordial(name: string) {
+        this.values[name] = {
+            "mutable": false,
+            "read": true
+        }
+    }
+
+    declare(nameToken: Token, mutable: boolean) {
+        const name = nameToken.lexeme;
+        if (this.isDeclared(name)) {
+            throw this.error(nameToken, `Duplicated declaration of '${name}'!`);
+        } else {
+            this.values[name] = {
+                "mutable": mutable,
+                "read": false
+            }
+        }
+    }
+
+}
