05b96a899bede2a4d6a82b83c61611d1d66eefe6
Kevin Li <kevinli020508@gmail.com>
2019-10-13 17:15:10 -0400

    Add functions for accessing List elements

--- List.ts
+++ List.ts
@@ -1,4 +1,6 @@
 import R from "ramda";
+import Decimal from "decimal.js";
+import { isNumber } from "./utils";
 
 const map = R.map;
 const filter = R.filter;
@@ -8,6 +10,26 @@ const reduce = R.reduce;
 const reduce_right = R.reduceRight;
 const all = R.all;
 const any = R.any;
+const first = R.head;
+const tail = R.tail;
+const head = R.init;
+const last = R.last;
+const nth = R.curry(function<T>(n: Decimal, list: readonly T[]) {
+    return R.nth(checkNumber(n), list);
+});
+const take = R.curry(function<T>(n: Decimal, list: readonly T[]) {
+    return R.take(checkNumber(n), list);
+});
+const take_last = R.curry(function<T>(n: Decimal, list: readonly T[]) {
+    return R.takeLast(checkNumber(n), list);
+});
+
+function checkNumber(n: any) {
+    if (isNumber(n)) {
+        return n.toNumber()
+    }
+    throw `List index must be a number!`;
+}
 
 export default {
     map,
@@ -17,5 +39,12 @@ export default {
     reduce,
     "reduce·right": reduce_right,
     all,
-    any
+    any,
+    first,
+    tail,
+    head,
+    last,
+    nth,
+    take,
+    "take·last": take_last
 };
