e3d0b8f4b03970573e0c5c6ae816fd8362dae273
Kevin Li <kevinli020508@gmail.com>
2019-10-13 16:53:09 -0400

    Add more List functions

--- List.ts
+++ List.ts
@@ -3,9 +3,19 @@ import R from "ramda";
 const map = R.map;
 const filter = R.filter;
 const reject = R.reject;
+const find = R.find;
+const reduce = R.reduce;
+const reduce_right = R.reduceRight;
+const all = R.all;
+const any = R.any;
 
 export default {
     map,
     filter,
     reject,
+    find,
+    reduce,
+    "reduce·right": reduce_right,
+    all,
+    any
 };
